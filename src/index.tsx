2b378863f0979b9d4284ac6b2b5831aac4c3cf67
金禅 <mark.ck@alibaba-inc.com>
2022-05-08 02:05:35 +0800

    feat: upgrade DesignerView in lowcode-engine

--- src/index.tsx
+++ src/index.tsx
@@ -2,132 +2,27 @@ import * as React from 'react';
 import { PureComponent } from 'react';
 
 import { config as engineConfig, common } from '@alilc/lowcode-engine'
-import { Asset } from '@alilc/lowcode-utils';
 import DragGhost from './drag-ghost';
 import './index.scss';
 
-const { designerCabin, editorCabin } = common;
-const { globalContext } = editorCabin;
-const { DesignerView, Designer } = designerCabin;
+const { designerCabin } = common;
+const { DesignerView } = designerCabin;
 
 export interface PluginProps {
 }
 
-interface DesignerPluginState {
-  componentMetadatas?: any[] | null;
-  library?: any[] | null;
-  extraEnvironment?: any[] | null;
-  renderEnv?: string;
-  device?: string;
-  locale?: string;
-  designMode?: string;
-  deviceClassName?: string;
-  simulatorUrl: Asset | null;
-  // @TODO 类型定义
-  requestHandlersMap: any;
-}
-
-export default class DesignerPlugin extends PureComponent<PluginProps, DesignerPluginState> {
+export default class DesignerPlugin extends PureComponent<PluginProps> {
   static displayName: 'LowcodePluginDesigner';
-
-  state: DesignerPluginState = {
-    componentMetadatas: null,
-    library: null,
-    extraEnvironment: null,
-    renderEnv: 'default',
-    device: 'default',
-    locale: '',
-    designMode: 'live',
-    deviceClassName: '',
-    simulatorUrl: null,
-    requestHandlersMap: null,
-  };
-
-  private _mounted = true;
-
-  constructor(props: any) {
-    super(props);
-    this.setupAssets();
-  }
-
-  private async setupAssets() {
-    try {
-      const editor = globalContext.get('editor');
-      const assets = await editor.onceGot('assets');
-      const renderEnv = engineConfig.get('renderEnv') || editor.get('renderEnv');
-      const device = engineConfig.get('device') || editor.get('device');
-      const locale = engineConfig.get('locale') || editor.get('locale');
-      const designMode = engineConfig.get('designMode') || editor.get('designMode');
-      const deviceClassName = engineConfig.get('deviceClassName') || editor.get('deviceClassName');
-      const simulatorUrl = engineConfig.get('simulatorUrl') || editor.get('simulatorUrl');
-      // @TODO setupAssets 里设置 requestHandlersMap 不太合适
-      const requestHandlersMap = engineConfig.get('requestHandlersMap') || editor.get('requestHandlersMap');
-      if (!this._mounted) {
-        return;
-      }
-      const { components, packages, extraEnvironment, utils } = assets;
-      const state = {
-        componentMetadatas: components || [],
-        library: packages || [],
-        utilsMetadata: utils || [],
-        extraEnvironment,
-        renderEnv,
-        device,
-        designMode,
-        deviceClassName,
-        simulatorUrl,
-        requestHandlersMap,
-        locale,
-      };
-      this.setState(state);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  componentWillUnmount() {
-    this._mounted = false;
-  }
-
-  private handleDesignerMount = (designer: Designer): void => {
-    const editor = globalContext.get('editor');
-    editor.set('designer', designer);
-    editor.emit('designer.ready', designer);
-    editor.onGot('schema', (schema) => {
-      designer.project.open(schema);
-    });
-  };
 
   render(): React.ReactNode {
     console.log('custom designer');
-    const editor = globalContext.get('editor');
-    const {
-      componentMetadatas,
-      utilsMetadata,
-      library,
-      extraEnvironment,
-      renderEnv,
-      device,
-      designMode,
-      deviceClassName,
-      simulatorUrl,
-      requestHandlersMap,
-      locale,
-    } = this.state;
-
-    if (!library || !componentMetadatas) {
-      // TODO: use a Loading
-      return null;
-    }
+    
     let deltaX;
     let deltaY;
     return (
       <DesignerView
         dragGhostComponent={DragGhost}
-        onMount={this.handleDesignerMount}
         className="lowcode-plugin-designer"
-        editor={editor}
-        designer={editor.get('designer')}
         onDragstart={(e) => {
           const { dragObject } = e;
           const node = dragObject?.nodes?.[0];
@@ -211,19 +106,6 @@ export default class DesignerPlugin extends PureComponent<PluginProps, DesignerP
           if (!engineConfig.get('rects')) {
             engineConfig.set('rects', rects);
           }
-        }}
-        componentMetadatas={componentMetadatas}
-        simulatorProps={{
-          library,
-          utilsMetadata,
-          extraEnvironment,
-          renderEnv,
-          device,
-          locale,
-          designMode,
-          deviceClassName,
-          simulatorUrl,
-          requestHandlersMap,
         }}
       />
     );
